1066fff65b75e945efa7065cee9ae6581f5e0073
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-01-14 16:45:52 +0200

    best price 1.0

--- app/js/index.js
+++ app/js/index.js
@@ -11,11 +11,11 @@
      zIndex: 1
  });
 
-
+ // my function -------------------------
  (function () {
      let mainNavLink = document.querySelectorAll('.main-nav__link');
      let mainNavLinkActive = 0;
-     const navMain = document.querySelector('.main-nav__ul');
+     const mainNav = document.querySelector('.main-nav__ul');
      // menu active -------------------------------
      mainNavLink.forEach(function (obj, i) {
          mainNavLink[i].addEventListener('click', function () {
@@ -28,7 +28,7 @@
      });
 
      // burger menu close ----------------------------
-     navMain.addEventListener('click', function (event) {
+     mainNav.addEventListener('click', function (event) {
          if (event.srcElement.tagName == "A" || event.srcElement.localName == "a") {
              document.querySelector('#main-nav-call').checked = false;
          }
@@ -36,10 +36,48 @@
      });
 
      // bg header ---------------
-     let color_black_opacity = '#00000070';
+     let color_header = 'rgba(20, 23, 35, 1)';
+     let boxShadow_header = '0 3px 6px rgba(255, 255, 255, 0.2)';
      let header = document.querySelector('.main-nav');
      window.addEventListener('scroll', function positionPoint() {
-         if (document.documentElement.scrollTop > header.clientHeight) header.style.backgroundColor = color_black_opacity;
-         else header.style.backgroundColor = 'transparent';
+         if (document.documentElement.scrollTop > header.clientHeight) {
+             header.style.backgroundColor = color_header;
+             header.style.boxShadow = boxShadow_header;
+         } else {
+             header.style.backgroundColor = 'transparent';
+             header.style.boxShadow = 'none';
+         }
      });
  })();
+
+
+ $('.price-slider').slick({
+     centerMode: true,
+     centerPadding: '60px',
+     //  autoplay: true,
+     //  autoplaySpeed: 1000,
+     //  speed: 500,
+     infinite: true,
+     slidesToShow: 3,
+     slidesToScroll: 3,
+     dots: true,
+     responsive: [{
+             breakpoint: 768,
+             settings: {
+                 //  arrows: false,
+                 centerMode: true,
+                 centerPadding: '40px',
+                 slidesToShow: 2
+             }
+         },
+         {
+             breakpoint: 480,
+             settings: {
+                 //  arrows: false,
+                 centerMode: true,
+                 centerPadding: '40px',
+                 slidesToShow: 1
+             }
+         }
+     ]
+ });
