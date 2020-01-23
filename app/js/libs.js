f0e8d98b7c70b87eb588e7607d8c3df855ce0e99
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-01-23 21:35:10 +0200

    form current data & hours

--- app/js/libs.js
+++ app/js/libs.js
@@ -15,6 +15,25 @@
         xhr.send();
     })();
 })();
+(function () {
+    let temp_date = new Date();
+    let day = temp_date.getDate();
+    let month = temp_date.getMonth() + 1;
+    let year = temp_date.getYear() + 1900;
+    let hours = temp_date.getHours();
+    let minutes = temp_date.getMinutes();
+    if (day < 10) {
+        day = "0" + day;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+    let currentDate = year + "-" + month + "-" + day;
+    let currentTime = hours + ":" + minutes;
+    document.querySelector('.book-date').setAttribute('value', currentDate);
+    document.querySelector('.book-time').setAttribute('value', currentTime);
+
+})();
 //
 //++++++++++++++ MAIN MENU ++++++++++++++++++++++++++++++++++++++++++++++
 (function () {
